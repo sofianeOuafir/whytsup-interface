c6e86de2e98d1e71ad5a5331a5ac9f98f03e08c8
Sofiane <sofiane.ouafir@live.fr>
2020-10-26 10:28:02 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ import Paper from "@material-ui/core/Paper";
 
 const Home = () => {
   return (
-    <Layout title="WhyUpWhyDown">
+    <Layout title="Assetify">
       <Paper>
         <Headlines />
       </Paper>
